68971f1e0135925d8a7d55279b46c9bd74e96cbb
Stephen Crane <cranes@google.com>
2022-02-09 12:51:11 -0800

    Add clang-tools to environment setup
    Adds the clang-tools repo binary dir to the build environment so we can
    use bindgen from this repo.
    
    Test: build.py
    Bug: 192472390
    Change-Id: I7ac838ef5381f0b9cabae14e2bde1e157541349b

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -30,6 +30,7 @@ gettop() {
 
 export TRUSTY_TOP=$(gettop)
 export CLANG_BINDIR=${TRUSTY_TOP}/prebuilts/clang/host/linux-x86/clang-r416183c1/bin
+export CLANG_TOOLS_BINDIR=${TRUSTY_TOP}/prebuilts/clang-tools/linux-x86/bin
 export LINUX_CLANG_BINDIR=${TRUSTY_TOP}/prebuilts/clang/host/linux-x86/clang-r433403/bin
 export RUST_BINDIR=${TRUSTY_TOP}/prebuilts/rust/linux-x86/1.59.0/bin
 export ARCH_arm_TOOLCHAIN_PREFIX=${TRUSTY_TOP}/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
